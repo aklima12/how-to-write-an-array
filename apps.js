ad3acfacadd7e9958277ea554852430a44041e52
aklima12 <128598790+aklima12@users.noreply.github.com>
2023-05-23 21:50:47 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,19 @@
+// var student={sname:"jamal miah",height:"5.11",education:"CSE",hobby:"cricket"};
+// document.getElementById('print').innerHTML=student.sname+"<br>"+student.height;
+// document.getElementById('print').style.color='red';
+// document.getElementById('print').style.fontsize='40px'
+
+
+// var sarray=["jamal","kamal","Rana","Rubel","Rasal"];
+// sarray.push("Aklima");
+// sarray.pop("Aklima");
+// document.write(sarray);
+
+
+function addstyle(){
+	document.getElementById('print').style.color='red';
+}
+
+function removestyle(){
+	document.getElementById('print').style.color='';
+}
